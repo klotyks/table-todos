e5aec2eaeec79d9f1e937bd3922fbcb85dd6486b
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-10-10 00:40:06 +0300

    Extracted: function sendRequest to module web

--- js/model.js
+++ js/model.js
@@ -1,12 +1,5 @@
 let todos = []
 
-async function sendRequest() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos/')
-  const text = await response.text()
-  const parsedText = JSON.parse(text)
-  return parsedText
-}
-
 function setTodos(newTodos) {
   todos = newTodos
 }
